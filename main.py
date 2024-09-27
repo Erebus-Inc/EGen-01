cae19639469c92bbd2c2a6d1167cf41568bfd578
ErebusTN <mouhebga62@gmail.com>
2024-09-27 21:04:20 +0100

    fix main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from fastapi import *
-from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 
 app = FastAPI(
     title="EGen-01",
