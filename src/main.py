bfb9f7a7decb49fbd35152d45658ccb5fb0dc028
ErebusTN <mouhebga62@gmail.com>
2024-09-28 11:12:34 +0100

    clean the main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI,APIRouter
-from dotenv import load_dotenv
+from fastapi import FastAPI
 from routes import base
 
-load_dotenv()
 
-app = FastAPI(
-    title="EGen-01",
-    description="",
-    version="0.1",
-    docs_url=None,
-    redoc_url=None
-)
-
+app = FastAPI()
 
 app.include_router(base.base_route)
