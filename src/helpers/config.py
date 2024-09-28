02757f9a93a44899bb3858d2ee35c72f16015611
ErebusTN <mouhebga62@gmail.com>
2024-09-28 13:15:29 +0100

    update config.py

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    # Define your settings here.
+    # Define app settings here.
     APP_NAME:str 
     APP_VERSION:str
     OPEN_AI_API:str
-    
+    # Define files settings here.
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
     class Config:
         env_file = ".env"
         
