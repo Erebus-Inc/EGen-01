cbb8ca12b77b6b0f43695626615c0ef07c6153dd
ErebusTN <mouhebga62@gmail.com>
2024-10-06 17:15:14 +0100

    update name file download system

--- src/controllers/dataControllers.py
+++ src/controllers/dataControllers.py
@@ -1,7 +1,9 @@
 from .baseControllers import baseController
+from .projectControllers import projectController
 from fastapi import UploadFile
 from Models import responseSignals
-
+import re
+import os
 class dataController(baseController):
     def __init__(self):
         super().__init__()
@@ -14,3 +16,23 @@ class dataController(baseController):
             return False , responseSignals.FILE_SIZE_EXCEEDED.value
         
         return True , responseSignals.FILE_VALIDATED_SUCCESS.value
+    
+    def name_Generate(self,org:str,project_id:str):
+        randomKey = self.random_string()
+        projectPath = projectController().getProjectPath(project_id=project_id)
+        new_file_Name = self.clean_file_name(org = org) 
+        new_file_path = os.path.join(projectPath,randomKey+"_"+new_file_Name)
+        
+        while os.path.exists(new_file_path):
+            randomKey = self.random_string()
+            new_file_path = os.path.join(projectPath,randomKey+"_"+new_file_Name)
+            
+        return new_file_path
+        
+    def clean_file_name(self,org:str):
+        new_file_Name = re.sub(r'[^\w.]','',org.strip())
+        
+        new_file_Name =  new_file_Name.replace("","_")
+        
+        return new_file_Name
+    
