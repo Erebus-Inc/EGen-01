886ad05df905eb0282f4ba9412b2f28efde5acb1
ErebusTN <mouhebga62@gmail.com>
2024-10-06 15:35:39 +0100

    update dataController.py outputs

--- src/controllers/dataControllers.py
+++ src/controllers/dataControllers.py
@@ -1,5 +1,6 @@
 from .baseControllers import baseController
 from fastapi import UploadFile
+from Models import responseSignals
 
 class dataController(baseController):
     def __init__(self):
@@ -7,9 +8,9 @@ class dataController(baseController):
         self.size_scale = 1048576
     def FILE_VALIDATOR(self,file:UploadFile):
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False , "File is not allowed to be uploaded the type is not supported"
+            return False , responseSignals.FILE_TYPE_NOT_SUPPORTED.value
         
         if file.size > self.app_settings.FILE_MAX_SIZE*self.size_scale:
-            return False , "File is too large to be uploaded "
+            return False , responseSignals.FILE_SIZE_EXCEEDED.value
         
-        return True , "File uploaded successfully."
+        return True , responseSignals.FILE_VALIDATED_SUCCESS.value
